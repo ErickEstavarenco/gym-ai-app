9cdfd011d7b1e0d7bfb30b45c5fe24b6c7023fe1
Erick Estavarenco <erickestavarenco@gmail.com>
2026-04-12 15:01:34 -0300

    chore: backend funcionando com auth, banco e erro pendente na IA

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -15,12 +15,10 @@ const {
   deleteExercise
 } = require("../controllers/exerciseController");
 
-/* WORKOUTS */
 router.post("/workouts", auth, createWorkout);
 router.get("/workouts", auth, getWorkouts);
 router.delete("/workouts/:id", auth, deleteWorkout);
 
-/* EXERCISES */
 router.post("/exercises", auth, createExercise);
 router.get("/exercises/:workout_id", auth, getExercises);
 router.delete("/exercises/:id", auth, deleteExercise);
